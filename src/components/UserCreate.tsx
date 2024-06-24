903224a4d37b4e1d3692b0e20f50be9b616f0b51
johyebin <hyebinjo91@joonggonara.co.kr>
2024-06-24 18:49:50 +0900

    [feat]: validation

--- src/components/UserCreate.tsx
+++ src/components/UserCreate.tsx
@@ -5,25 +5,48 @@ import {
   NumberInput,
   SimpleForm,
   TextInput,
+  choices,
+  email,
+  maxLength,
+  minLength,
+  minValue,
+  number,
+  regex,
+  required,
 } from "react-admin";
 
-export const UserCreate = () => (
-  <Create redirect="list">
-    <SimpleForm>
-      <TextInput source="name" />
-      <TextInput source="username" />
-      <TextInput source="description" fullWidth multiline />
-      <TextInput source="address.street" />
-      <NumberInput source="phone" />
-      <TextInput source="website" />
-      <TextInput source="company.name" />
-      <FileInput
-        source="files"
-        placeholder={<p>Drop your file here</p>}
-        multiple
-      >
-        <ImageField source="src" title="title" />
-      </FileInput>
-    </SimpleForm>
-  </Create>
-);
+export const UserCreate = () => {
+  const validateName = [
+    required("필수항목입니다."),
+    minLength(2),
+    maxLength(15),
+  ];
+  const validateEmail = email();
+  const validatePhone = [number(), minValue(18)];
+  const validateZipCode = regex(/^\d{5}$/, "Must be a valid Zip Code");
+  const validateGender = choices(
+    ["m", "f", "nc"],
+    "Please choose one of the values"
+  );
+
+  return (
+    <Create redirect="list">
+      <SimpleForm>
+        <TextInput source="name" validate={validateName} />
+        <TextInput source="username" validate={required()} />
+        <TextInput source="description" fullWidth multiline />
+        <TextInput source="address.street" />
+        <NumberInput source="phone" validate={validatePhone} />
+        <TextInput source="website" />
+        <TextInput source="company.name" />
+        <FileInput
+          source="files"
+          placeholder={<p>Drop your file here</p>}
+          multiple
+        >
+          <ImageField source="src" title="title" />
+        </FileInput>
+      </SimpleForm>
+    </Create>
+  );
+};
